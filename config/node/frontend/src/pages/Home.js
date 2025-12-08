bb243838168b8de96926cd66faebde85ec248d15
peenk1 <mastalski102@gmail.com>
2025-12-08 19:59:05 +0100

    feat: input of home component and style for it

--- config/node/frontend/src/pages/Home.js
+++ config/node/frontend/src/pages/Home.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import {Typography, Button} from "@mui/material";
 
 function Home(props) {
     return (
-        <div>strona internetowa</div>
+        <div className = 'home'>
+            <h1 className = 'home__title'>GEOPORTAL</h1>
+
+            <Typography className='home__subtile'>
+                Geoportal tematyczny poswiecony danym przestrzennym.
+            </Typography>
+
+            <Button className='home__button' variant='contained' size='large'>START</Button>
+        </div>
     );
 }
 
