8b60d0b5b4732e4f8b0b7d21746cdcd889542e5b
peenk1 <mastalski102@gmail.com>
2025-12-17 12:53:26 +0100

    feat: fastapi not working

--- config/fastapi/app/routers/db_insert.py
+++ config/fastapi/app/routers/db_insert.py
@@ -1,18 +1,20 @@
 from fastapi import APIRouter
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
-router = APIRouter()
+from app.settings import db_name, db_user, db_password
 
-def connect_to_db():
+router_insert = APIRouter()
+
+def connect_to_db(db_name: str, db_user: str, db_password: str):
     return create_engine(
-        f"postgresql://postgres:postgres@postgis:5432/postgres"
+        f"postgresql://{db_user}:{db_password}@postgis:5432/{db_name}"
     )
 
-@router_insert.get("/import_user")
+@router_insert.get("/insert_user")
 async def insert_user():
 
     try:
-        db_connection = connect_to_db()
+        db_connection = connect_to_db(db_name=db_name, db_user=db_user, db_password=db_password)
 
         sql_query = """
                     inser into users (name, posts, location)
