3a04900a8589347db4468f81f792f9dde9680181
peenk1 <mastalski102@gmail.com>
2026-01-12 14:13:03 +0100

    feat: add cardComponent

--- config/node/frontend/src/pages/ListOfItems.js
+++ config/node/frontend/src/pages/ListOfItems.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import UserCard from "../components/UserCard";
 
 function ListOfItems(props) {
+
+    const [users, setUsers] = useState([]);
+    // https://jsonplaceholder.typicode.com/users
+    useEffect(() => {
+        // http://localhost:10000/app/get_users
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(res => {
+                console.log(res);
+                setUsers(res)
+            })
+
+        console.log('działa', users)
+    }, [])
+
+
     return (
-        <div>List of items</div>
+        <div>
+            List of items
+            <div>
+                {/*{users.data?.map(user => <UserCard user={user}/>)}*/}
+                {users?.map(user => <UserCard user={user}/>)}
+            </div>
+        </div>
     );
 }
 
