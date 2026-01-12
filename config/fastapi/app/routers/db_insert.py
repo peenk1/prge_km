57f3bdf92243f507d8a20fd69e4db1c4d9bdab48
peenk1 <mastalski102@gmail.com>
2026-01-12 12:53:42 +0100

    feat: add dynamic requests to backend

--- config/fastapi/app/routers/db_insert.py
+++ config/fastapi/app/routers/db_insert.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 from sqlalchemy import create_engine, text
 
 from app.settings import db_name, db_user, db_password
@@ -10,14 +11,24 @@ def connect_to_db(db_name: str, db_user: str, db_password: str):
         f"postgresql://{db_user}:{db_password}@postgis:5432/{db_name}"
     )
 
-@router_insert.get("/insert_user")
-async def insert_user():
+class UserData(BaseModel):
+    name: str
+    posts: int
+    location: str
 
+@router_insert.post("/insert_user")
+async def insert_user(user: UserData ):
     try:
         db_connection = connect_to_db(db_name=db_name, db_user=db_user, db_password=db_password)
 
-        sql_query = """
-                    inser into users (name, posts, location)
+        params = {
+            "name": user.name,
+            "posts": user.posts,
+            "location": user.location
+        }
+
+        sql_query = text("""
+                    insert into users (name, posts, location)
                     values ('Jan', 3, 'Gdańsk'); \
                     """)
 
@@ -30,10 +41,6 @@ async def insert_user():
     except Exception as e:
         print(e)
         raise e
-
-
-
-
 
 
     return {'status': 1}
